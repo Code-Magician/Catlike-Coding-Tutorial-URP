046687850efde0444e6cba8c0a83b96208e2ee7e
temp-Priyansh-Athena <athena.priyansh@gmail.com>
2026-02-01 14:48:08 +0530

    2.2 Basics - Clock

---
+++ Assets/Catlike_Clock/Scripts/Clock.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] GameObject hourIndicatorPrefab;
+    [SerializeField] Transform  hourArmPivot, minuteArmPivot, secondArmPivot;
+
+    float hoursToDegree = -360f / 12, minutesToDegree = -360f / 60, secondsToDegree = -360f / 60;
+
+    private void Awake()
+    {
+        PlaceHourIndicator();
+    }
+
+    private void Update()
+    {
+        RunClock();
+    }
+
+    private void PlaceHourIndicator()
+    {
+        for(int i=30; i<=360; i+=30)
+        {
+            Vector3 pos = new Vector3(0, 0, -0.25f);
+            pos.x = Mathf.Sin(Mathf.Deg2Rad * i) * 4f;
+            pos.y = Mathf.Cos(Mathf.Deg2Rad * i) * 4f;
+
+            GameObject hourIndicator = Instantiate(hourIndicatorPrefab, pos, Quaternion.Euler(0, 0, -i), transform);
+            hourIndicator.name = $"Hour Indicator {i / 30}";
+        }
+    }
+
+    private void RunClock()
+    {
+        var currentDateTime = DateTime.Now.TimeOfDay;
+
+        hourArmPivot.localRotation = Quaternion.Euler(0, 0, hoursToDegree * (float) currentDateTime.TotalHours);
+        minuteArmPivot.localRotation = Quaternion.Euler(0, 0, minutesToDegree * (float) currentDateTime.TotalMinutes);
+        secondArmPivot.localRotation = Quaternion.Euler(0, 0, secondsToDegree * (float) currentDateTime.TotalSeconds);
+    }
+}
